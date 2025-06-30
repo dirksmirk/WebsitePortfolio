36c4e2e889c22cd3cae9dffc75475f307576e7ac
Dirk Hubrecht <dirk.holger.hubrecht@gmail.com>
2025-07-01 01:15:06 +0200

    modal fix, tailwindcss cleanup

--- src/pages/ProjectDetails.tsx
+++ src/pages/ProjectDetails.tsx
@@ -9,7 +9,7 @@ import {
   ArrowUpRight,
 } from "lucide-react";
 import { useState } from "react";
-import ImageModal from "@/components/Modal";
+import ImageModal from "@/components/ImageModal";
 
 const ProjectDetail = () => {
   const { slug } = useParams();
@@ -245,7 +245,7 @@ const ProjectDetail = () => {
               {project.link && (
                 <button
                   onClick={() => window.open(project.link, "_blank")}
-                  className="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-6 rounded-md px-3"
+                  className="inline-flex items-center justify-center gap-2 whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-6 rounded-md"
 
                 >
                   <ArrowUpRight className="w-4 h-4" />
